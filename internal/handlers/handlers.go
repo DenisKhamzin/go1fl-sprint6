5ff98df1dc1f221afac36f941e2b2b361426ff79
Denis Khamzin <96993113+DenisKhamzin@users.noreply.github.com>
2026-05-10 16:13:56 +0300

    server works, correct file created

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
 )
 
 func SimpleGetHandler(res http.ResponseWriter, req *http.Request) {
@@ -21,14 +25,12 @@ func Uploader(res http.ResponseWriter, req *http.Request) {
 	file, _, err := req.FormFile("myFile")
 	if err != nil {
 		http.Error(res, "Error retrieving file", http.StatusBadRequest)
-		fmt.Println(req.Header)
 		return
 	}
 	defer file.Close()
 	content, err := io.ReadAll(file)
 	if err != nil {
 		http.Error(res, "Error reading file", http.StatusInternalServerError)
-		fmt.Println("2")
 		return
 	}
 	fmt.Println("Файл загружен")
@@ -36,4 +38,26 @@ func Uploader(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	res.WriteHeader(http.StatusOK)
 	res.Write(content)
+	// создаем файл
+	text := service.Converter(string(content))
+	err = fileCreator(text)
+	if err != nil {
+		fmt.Printf("Error: %s\n, err")
+	}
+
+}
+
+func fileCreator(content string) error {
+	now := time.Now()
+	fileName := now.Format("2006-01-02 15:04:05") + ".txt"
+	file, err := os.Create(fileName)
+	if err != nil {
+		fmt.Printf("Creating error: %s\n", err)
+	}
+	defer file.Close()
+	_, err = file.WriteString(content)
+	if err != nil {
+		fmt.Printf("Writing error: %s\n", err)
+	}
+	return nil
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,17 @@
 package service
+
+import "github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+
+func Converter(content string) string {
+	isMorse := true
+	for _, char := range content {
+		if char != rune('.') && char != rune('-') && char != rune(' ') {
+			isMorse = false
+			break
+		}
+	}
+	if isMorse {
+		return morse.ToText(content)
+	}
+	return morse.ToMorse(content)
+}
